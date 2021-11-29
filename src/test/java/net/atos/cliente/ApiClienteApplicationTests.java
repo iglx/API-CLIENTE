eb61db8fb749c2e4413b1aa309f50afd4e8a3dbb
Xavier <igor.xavier@atos.net>
2021-11-29 09:16:48 -0300

    API-CLIENTE: Configurando o repositório

---
+++ src/test/java/net/atos/cliente/ApiClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atos.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
